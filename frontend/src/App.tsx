65c85a04c8f4d928dba8ed0666ffc7ea95738829
Amin Amouhadi <aminamouhadi@Amins-Mac-mini.local>
2025-05-03 22:28:29 -0700

    Remove Navbar from landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,35 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Login from './components/Login'
 import Signup from './components/Signup'
 import Navbar from './components/Navbar'
 import HomePage from './components/HomePage'
 
+// Helper component to conditionally display Navbar
+const NavbarWrapper = ({ children }: { children: React.ReactNode }) => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+  
+  return (
+    <>
+      {!isHomePage && <Navbar />}
+      {children}
+    </>
+  );
+};
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </div>
+      <NavbarWrapper>
+        <div className="App">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
+      </NavbarWrapper>
     </Router>
   )
 }
